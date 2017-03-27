fc33758f11bb07a652f645a6e80701e57333ad00
trevormac <trevor.c.macgregor@gmail.com>
2017-03-27 16:28:25 -0400

    Initial Commit

---
+++ Github_Repos/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Github_Repos
+//
+//  Created by Trevor MacGregor on 2017-03-27.
+//  Copyright © 2017 Trevor MacGregor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
